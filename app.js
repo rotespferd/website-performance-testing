c0bf51f5343df3db22852458006af0de63afa1b4
Marian Sievers <marian.sievers@gmail.com>
2015-03-28 14:09:58 +0100

    Refactore the phantomjs script

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 var page = require('webpage').create(),
-  system = require('system'),
-  t, address;
+system = require('system');
 
-if (system.args.length === 1) {
-  console.log('Usage: loadspeed.js <some URL>');
-  phantom.exit();
-}
 
-t = Date.now();
-address = system.args[1];
+// set parameters
+
+var address = system.args[1];
+
+console.log("Test the address " + address);
+
+var time = Date.now();
 page.open(address, function(status) {
-  if (status !== 'success') {
-    console.log('FAIL to load the address');
-  } else {
-    t = Date.now() - t;
-    console.log('Loading ' + system.args[1]);
-    console.log('Loading time ' + t + ' msec');
-  }
-  phantom.exit();
+    if (status !== 'success') {
+        console.log('FAIL to load the address');
+    } else {
+        time = Date.now() - time;
+        console.log(time);
+    }
+    phantom.exit();
 });
